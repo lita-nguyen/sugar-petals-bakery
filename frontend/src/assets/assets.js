d68b49b442c92cbd7335656da216d7dd8b88723c
Lita Nguyen <nguyenlenhatlinh512@gmail.com>
2026-04-03 17:06:36 +0700

    complete navbar

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -1,12 +1,15 @@
 import logo from './logo.png'
 import logo1 from './logo1.png'
 
+import img_header from './header_img.jpg'
+
 import icon_search from './icon-search.png'
-import icon_basket from './icon-basket.png'
+import icon_basket from './icon-basket.jpg'
 
 export const assets = {
     logo,
     logo1,
+    img_header,
     icon_search,
     icon_basket
 }
--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,17 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Navbar.css'
 import { assets } from '../../assets/assets'
 
 const Navbar = () => {
+
+    const [menu, setMenu] = useState("Home");
+
   return (
     <div className='navbar'>
-      <img src={assets.logo} alt="Logo" className="logo"/>
-
+      <div className="navbar-left">
+        <img src={assets.logo} alt="Logo" className="logo"/>
+        <div className="site-info">
+          <h1 className="site-name">Sugar Petals</h1>
+          <p className="site-slogan">Delicate bites, blooming delights</p>
+        </div>
+      </div>
+          
       <ul className='navbar-menu'>
-        <li>Home</li>
-        <li>Menu</li>
-        <li>Mobile-app</li>
-        <li>Contact us</li>
+        <li className={menu === "Home" ? "active" : ""} onClick={() => setMenu("Home")}>Home</li>
+        <li className={menu === "Menu" ? "active" : ""} onClick={() => setMenu("Menu")}>Menu</li>
+        <li className={menu === "Mobile" ? "active" : ""} onClick={() => setMenu("Mobile")}>Mobile-app</li>
+        <li className={menu === "Contact" ? "active" : ""} onClick={() => setMenu("Contact")}>Contact us</li>
       </ul>
 
       <div className="navbar-right">
