268be83ccf5236202fcd64f4f12185b7ffc73cfc
Lita Nguyen <nguyenlenhatlinh512@gmail.com>
2026-04-04 14:01:53 +0700

    explore menu

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -6,6 +6,15 @@ import img_header from './header_img.jpg'
 import icon_search from './icon-search.png'
 import icon_basket from './icon-basket.jpg'
 
+import menu_1 from './menu_1.jpg'
+import menu_2 from './menu_2.jpg'
+import menu_3 from './menu_3.jpg'
+import menu_4 from './menu_4.jpg'
+import menu_5 from './menu_5.jpg'
+import menu_6 from './menu_6.jpg'
+import menu_7 from './menu_7.jpg'
+import menu_8 from './menu_8.jpg'
+
 export const assets = {
     logo,
     logo1,
@@ -13,3 +22,38 @@ export const assets = {
     icon_search,
     icon_basket
 }
+
+export const menulist = [
+    {
+        menu_name: "Cake",
+        menu_image: menu_1
+    },
+    {
+        menu_name: "Cupcake",
+        menu_image: menu_2
+    },
+    {
+        menu_name: "Croissant",
+        menu_image: menu_3
+    },
+    {
+        menu_name: "Cheesecake",
+        menu_image: menu_4
+    },
+    {
+        menu_name: "Donut",
+        menu_image: menu_5
+    },
+    {
+        menu_name: "Cookies",
+        menu_image: menu_6
+    },
+    {
+        menu_name: "Mousse",
+        menu_image: menu_7
+    },
+    {
+        menu_name: "Sweet Buns",
+        menu_image: menu_8
+    }
+]
--- frontend/src/components/ExploreMenu/ExploreMenu.jsx
+++ frontend/src/components/ExploreMenu/ExploreMenu.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
 import './ExploreMenu.css'
+import { menulist } from '../../assets/assets'
 
-const ExploreMenu = () => {
+const ExploreMenu = ({category, setCategory}) => {
+
   return (
-    <div>
-      
+    <div className='explore-menu' id='explore-menu'>
+      <h1>Explore our menu</h1>
+      <p className='explore-menu-text'>
+        Explore our diverse menu of delicious baked goods and sweet treats. 
+        From classic pastries to delightful desserts, pick your favorite and enjoy the perfect flavor today!
+      </p>
+      <div className="explore-menu-list">
+        {menulist.map((item, index) => {
+          return (
+            <div onClick={()=>setCategory(prev=>prev===item.menu_name?"All":item.menu_name)} key={index} className='explore-menu-list-item'>
+              <img className={category===item.menu_name?"active":""} src={item.menu_image} alt="" />
+              <p>{item.menu_name}</p>
+            </div>
+          )
+        })}
+      </div>
+
+      <hr />
     </div>
   )
 }
