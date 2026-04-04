ed7946dddbb499219a2a911b88c1abfdbad6a58b
Lita Nguyen <nguyenlenhatlinh512@gmail.com>
2026-04-04 12:19:26 +0700

    hearder & content

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -5,8 +5,12 @@ const Header = () => {
   return (
     <div className='header'>
       <div className="header-contents">
-        <h2>Order your favourite food here</h2>
-        <p>Delicious treats delivered to your doorstep</p>
+        <h2>A Bloom of Sweet Elegance</h2>
+        <p>
+          At Sugar Petals, we transform simple ingredients into delicate works of edible art. 
+          Each creation blends flavor and beauty in perfect harmony, crafted to delight your senses. 
+          Let every bite bring a touch of sweetness and a blooming moment of joy to your day.
+          </p>
         <button>View Menu</button>
       </div>
     </div>
