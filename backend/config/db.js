86f3c78b09d989fb86f3675ad56f734b9772adbb
Phan Tiến Thnh <phantienthinh0907@gmail.com>
2026-04-09 08:47:09 +0700

    Configure for deployment

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://nhatlinh:1bqEUNc4y12d51I0@cluster0.1c2yqih.mongodb.net/sugar-petals-bakery').then(()=>console.log("DB Connected"));
+    await mongoose.connect(process.env.MONGODB_URI).then(()=>console.log("DB Connected"));
 }
