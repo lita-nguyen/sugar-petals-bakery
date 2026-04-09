c5b0fc6f55aa2d4685f34ac5e410b7ac95a31cee
Phan Tiến Thnh <phantienthinh0907@gmail.com>
2026-04-09 08:05:33 +0700

    Update bug

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,18 +1,13 @@
 import orderModel from "../models/orderModel.js";
 import userModel from "../models/userModel.js";
 
-<<<<<<< HEAD
 // Đặt hàng
-=======
-// placing user order (COD)
->>>>>>> a3610f3193046d40373a7aa75c6dfc2a9ef4a079
 const placeOrder = async (req, res) => {
     try {
         const newOrder = new orderModel({
             userId: req.body.userId,
             items: req.body.items,
             amount: req.body.amount,
-<<<<<<< HEAD
             address: req.body.address,
         });
         await newOrder.save();
@@ -32,26 +27,10 @@ const userOrders = async (req, res) => {
     try {
         const orders = await orderModel.find({ userId: req.body.userId });
         res.json({ success: true, data: orders });
-=======
-            address: req.body.address
-        });
-
-        await newOrder.save();
-
-        await userModel.findByIdAndUpdate(req.body.userId, { cartData: {} });
-
-        res.json({
-            success: true,
-            message: "Order Placed Successfully",
-            orderId: newOrder._id
-        });
-
->>>>>>> a3610f3193046d40373a7aa75c6dfc2a9ef4a079
     } catch (error) {
         console.log(error);
         res.json({ success: false, message: "Error" });
     }
-<<<<<<< HEAD
 }
 
 // Lấy tất cả đơn hàng (admin)
@@ -77,8 +56,3 @@ const updateStatus = async (req, res) => {
 }
 
 export { placeOrder, userOrders, listOrders, updateStatus }
-=======
-};
-
-export { placeOrder };
->>>>>>> a3610f3193046d40373a7aa75c6dfc2a9ef4a079
