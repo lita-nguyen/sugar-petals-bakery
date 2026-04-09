1a5c881478da69d6ec927e4d4c60a1767df9a215
Phan Tiến Thnh <phantienthinh0907@gmail.com>
2026-04-09 08:09:40 +0700

    fix: resolve merge conflicts in order files

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import mongoose from "mongoose";
 
 const orderSchema = new mongoose.Schema({
@@ -13,20 +12,3 @@ const orderSchema = new mongoose.Schema({
 
 const orderModel = mongoose.models.order || mongoose.model("order", orderSchema);
 export default orderModel;
-=======
-import mongoose from "mongoose"
-
-const orderSchema = new mongoose.Schema({
-    userId:{type:String,required:true},
-    items:{type:Array,required:true},
-    amount:{type:Number,required:true},
-    address:{type:Object,required:true},
-    status:{type:String,default:"Food Processing"},
-    date:{type:Date,default:Date.now()},
-    payment:{type:Boolean,default:false}
-})
-
-const orderModel = mongoose.models.order || mongoose.model("order",orderSchema);
-
-export default orderModel;
->>>>>>> a3610f3193046d40373a7aa75c6dfc2a9ef4a079
